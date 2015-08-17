f166bf0a06b47a1fe84c93e56a7fb90413716176
Theblood <bastiannovoa123@gmail.com>
2015-08-16 21:10:20 -0300

    Corregido los Espacios por +
    ya no debera escribir Pedro+Perez Ahora simplemente Escriba Pedro Perez

--- Rutificador.sh
+++ Rutificador.sh
@@ -36,19 +36,12 @@ echo  "Tu Sistema es:"
 echo "$kernel"
 echo "------------------------------------------------------------------"
 novato=$(whoami)
-echo "Holas querido y amado $novato , espero que estes bien  bueno a 
-lo que vamos  para  poder cursar este script tienes que  remplazar cada 
-espacio por + por ejemplo Pedrito+Perez, si bien arriba se explica la 
-forma  de buscar a una persona por su nombretambien se puede buscar 
-por su numero de rut sin el digito verificador es decir escribir 
-el rut antes del guion quedaria de esta forma:12345678"
-echo "------------------------------------------------------------------"
-echo "Cabe destacar que estos datos son  sacados de otros sitios por lo cual 
+echo "Holas Tu $novato ,Cabe destacar que estos datos son  sacados de otros sitios por lo cual 
 no se vulnera ninguna politica, eticamente esto esta penado en otros lugares
 no obstante la herramienta es personal y privada , de codigo libre, si 
 vas a copiar/pegar por favor poneme en los comentarios! Saludos! "
 echo "---------------------------------------------"
 echo "Introduce Tu Nombre o Rut : "
 read rut 
-curl 'http://chile.rutificador.com/get_generic_ajax/' -H 'Host: chile.rutificador.com' -H 'User-Agent: Mozilla/5.0 (X11; Linux i686; rv:31.0) Gecko/20100101 Firefox/31.0 Iceweasel/31.8.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Referer: http://chile.rutificador.com/' -H 'Cookie: _ga=GA1.2.772111381.1438997737; csrftoken=wcMGps9A4SvNsk1dKWsxXKa3nqX6BB0f; _gat=1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data 'csrfmiddlewaretoken=wcMGps9A4SvNsk1dKWsxXKa3nqX6BB0f&entrada='$rut --silent | grep }| python -mjson.tool
+curl 'http://chile.rutificador.com/get_generic_ajax/' -H 'Host: chile.rutificador.com' -H 'User-Agent: Mozilla/5.0 (X11; Linux i686; rv:31.0) Gecko/20100101 Firefox/31.0 Iceweasel/31.8.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Referer: http://chile.rutificador.com/' -H 'Cookie: _ga=GA1.2.772111381.1438997737; csrftoken=wcMGps9A4SvNsk1dKWsxXKa3nqX6BB0f; _gat=1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data 'csrfmiddlewaretoken=wcMGps9A4SvNsk1dKWsxXKa3nqX6BB0f&entrada='$nombre1"+"$nombre2"+"$apellido1"+"$apellido2 --silent | grep }| python -mjson.tool
 
